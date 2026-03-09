904b64273ee96b81193906a7382dc08891efb317
Joerg Gittinger <joerg.gittinger@etas.com>
2026-03-09 11:22:25 +0100

    qnx8 rpi4: fix mount script typo

--- qnx8/boards/rpi4-arm64/mount-fs.sh
+++ qnx8/boards/rpi4-arm64/mount-fs.sh
@@ -7,7 +7,7 @@ if [ -b /dev/sd0.ms.0 ]; then
     # GPT style partition
     echo "Mounting /boot partition /dev/sd0.ms.0"
     mount -tdos /dev/sd0.ms.0 /boot
-elif [ -b /dev/sd0t12 ]; then´
+elif [ -b /dev/sd0t12 ]; then
 	# MBR style partition
     echo "Mounting /boot partition /dev/sd0t12"
     mount -tdos /dev/sd0t12 /boot
@@ -16,7 +16,7 @@ fi
 
 # Mount S-CORE partition to /opt/score
 # Mount probe list: sd0t... is MBR, sd0.qnx6.x is GPT
-MOUNT_PROBE_LIST="sd0t177 sd0.qnx6.0 sd0.qnx6.1"
+MOUNT_PROBE_LIST="sd0t177 sd0.qnx6.1"
 MOUNT_POINT=/opt/score
 
 if [ -f /boot/etc/settings/mount ]; then
